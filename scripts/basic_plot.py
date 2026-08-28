a7fe0ca4b590715f70b96e921a73c5db21fbebda
John Bonini <bonini.physics@gmail.com>
2026-08-28 14:04:49 -0400

    reproduce data with poetry env
    this didn't use the nix env, but just save data with poetry for
    comparison after we switch

--- scripts/basic_plot.py
+++ scripts/basic_plot.py
@@ -2,13 +2,19 @@
 
 import os
 import numpy as np
-from qedmft.io import adiabatic_dat_from_p4vsp
 from qedmft import adiabatic as qad
 from qedmft.units import *
 from qedmft.plotting import plot_f_vs_lambdas_withchar
 
-base_dir = "/mnt/home/jbonini/ceph/photon_DMFT/linear_gamma/BN/norelax/"
-dat = adiabatic_dat_from_p4vsp(os.path.join(base_dir, "chi_0/a0"))
+# To use directory containing vaspout.h5 provided as cli arg:
+# import sys
+# from qedmft.io import adiabatic_dat_from_p4vsp
+# dat = adiabatic_dat_from_p4vsp(sys.argv[1])
+## dat.save_json("./bn_resp_example.json") # saves needed data to json file
+
+# load the json example data
+fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bn_resp_example.json")
+dat = qad.AdiabaticMatter.from_json(fn)
 
 phon = qad.hmat_to_freqs(dat.cmat, dat.masses_flat)
 f = phon[-2]
@@ -23,8 +29,10 @@ llambdas = np.linspace(0, 0.25, 1000)
 phts = photons_norm.range_scale_lambda(llambdas)
 freqs, displs, irvs, irns = qad.solve_all_qad(phts, dat)
 pht_part_all = displs[:, :, -photons_norm.nmodes :]
-chars = qad.pht_char(displs, nmodes)
+chars = qad.pht_char(displs, phts)
 
 plt, fig, ax = plot_f_vs_lambdas_withchar(freqs, chars, llambdas)
 ax.annotate("BN", (0.01, 0.95), xycoords="axes fraction")
 ax.set_ylim((0, 250))
+## fig.savefig("./bn_example.png")
+plt.show()
