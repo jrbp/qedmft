c2aa92f868a1c612e9b537d42710bde410f87a06
John Bonini <bonini.physics@gmail.com>
2026-08-28 14:24:08 -0400

    add some comments and cleanup of basic_plot script

--- scripts/basic_plot.py
+++ scripts/basic_plot.py
@@ -2,6 +2,7 @@
 
 import os
 import numpy as np
+
 from qedmft import adiabatic as qad
 from qedmft.units import *
 from qedmft.plotting import plot_f_vs_lambdas_withchar
@@ -16,21 +17,26 @@ from qedmft.plotting import plot_f_vs_lambdas_withchar
 fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bn_resp_example.json")
 dat = qad.AdiabaticMatter.from_json(fn)
 
-phon = qad.hmat_to_freqs(dat.cmat, dat.masses_flat)
-f = phon[-2]
-nharm = 1
-h0 = f
-fs = np.array([h0 * i for i in range(1, nharm + 1)])
+# setup cavity parameters
+phon = qad.hmat_to_freqs(dat.cmat, dat.masses_flat) # get the bare phonon freqs
+h0 = phon[-2] # fundamental cavity mode freq (here chosen from phonons to be in resonance)
+nharm = 1 # number of harmonics to include
+fs = np.array([h0 * i for i in range(1, nharm + 1)]) # set of mode frequencies
 ds = np.zeros((nharm, 3))
-ds[:, 0] = 1
+ds[:, 0] = 1 # normalized lambda vectors for each mode
+
+# photon modes with normalized lambdas
 photons_norm = qad.PhotonModes(freqs=fs, lambdas=ds)
 llambdas = np.linspace(0, 0.25, 1000)
 
-phts = photons_norm.range_scale_lambda(llambdas)
+# photon modes over the desired lambda range
+phts = photons_norm.range_scale_lambda(llambdas) # set of photon configurations for all 
+
+# solve for each lambda
 freqs, displs, irvs, irns = qad.solve_all_qad(phts, dat)
-pht_part_all = displs[:, :, -photons_norm.nmodes :]
 chars = qad.pht_char(displs, phts)
 
+# plot
 plt, fig, ax = plot_f_vs_lambdas_withchar(freqs, chars, llambdas)
 ax.annotate("BN", (0.01, 0.95), xycoords="axes fraction")
 ax.set_ylim((0, 250))
